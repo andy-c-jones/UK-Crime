adb1c7f5166a0630a8dfbc3f32fc20130a2798cc
Andrew <andrew_jones@outlook.com>
2013-04-04 21:37:00 +0100

    Added call to police api and fills table

--- QueryPoliceAPI.php
+++ QueryPoliceAPI.php
@@ -1,4 +1,12 @@
 <?php
 	$input = $_GET["location"];
-	echo $input;
+
+	$url = "http://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2011-08";
+
+	$curl = curl_init();
+    curl_setopt($curl, CURLOPT_HTTPGET, 1);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    echo $returnedJson = curl_exec($curl);
 ?>
